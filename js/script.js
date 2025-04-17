3a214a2a209bce50b974abe552beac3e3adee9ba
Andrey Shutyuk <andrewshoot@yandex.ru>
2025-04-17 16:53:05 +0300

    change structure + email

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function openEncodedLink() {
+  const encoded = 'aHR0cHM6Ly90Lm1lL2l2amV0';
+  const url = atob(encoded);
+  window.open(url, '_blank');
+}
+
+function openWA() {
+  const encoded = 'NzkwMzY2NjE1MTM=';
+  const phone = atob(encoded);
+  window.open(`https://wa.me/${phone}`, '_blank');
+}
+
+function sendEmail() {
+  window.location.href = "mailto:ivan@upcast.pro" + 
+                        "?subject=Запрос с сайта UPCAST" +
+                        "&body=Здравствуйте, Иван.%0D%0A%0D%0AМеня интересует...";
+}
+
+// Автоматическое обновление года
+document.getElementById('current-year').textContent = new Date().getFullYear();
